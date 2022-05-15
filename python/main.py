fb15e03b2757033610b7a6ba2ac269adc5dbb9fa
miku_note_win <miku.f.810129@gmail.com>
2022-05-15 13:55:21 +0900

    step3-2 done

--- python/main.py
+++ python/main.py
@@ -1,6 +1,7 @@
 import os
 import logging
 import pathlib
+import json
 from fastapi import FastAPI, Form, HTTPException
 from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
@@ -23,9 +24,25 @@ def root():
     return {"message": "Hello, world!"}
 
 @app.post("/items")
-def add_item(name: str = Form(...)):
-    logger.info(f"Receive item: {name}")
-    return {"message": f"item received: {name}"}
+def add_item(name: str = Form(...),category: str = Form(...)):
+    # Read json
+    if not os.path.isfile('items.json'):
+        read_data = {"items":[]}
+    else:
+        read_file=open('items.json','r')
+        read_data=json.load(read_file)
+        read_file.close()
+    # Add the data
+    new_data = {"name":name,"category":category}
+    read_data["items"].append(new_data)
+
+    # Record new data
+    j_read=open('items.json','w')
+    json.dump(read_data,j_read,indent=2)
+    j_read.close()
+
+    logger.info(f"Receive item: {name}, {category}")
+    return {"message": f"item received: {name}, category: {category}"}
 
 @app.get("/image/{items_image}")
 async def get_image(items_image):
